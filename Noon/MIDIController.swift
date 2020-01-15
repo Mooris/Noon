b0ebe6b20636c7b625397c836addd39148b3449c
Mooris <peloille.pierre@gmail.com>
2020-01-15 08:07:00 +0100

    Commented code for MIDIThru and stop getting the 6th interface but find the first one with 4 dests (well, that's not what I call "good" but is still better)

--- Noon/MIDIController.swift
+++ Noon/MIDIController.swift
@@ -23,51 +23,52 @@ class MIDIController {
     private var _devices: [MIDIDevice] = []
     private var _ref = MIDIClientRef()
     private var _edp = MIDIEndpointRef()
-    private var _thruParams = MIDIThruConnectionParams()
-    private var _ids: MIDIUniqueID = 0xFF69b4
-    private var _conRef = MIDIThruConnectionRef()
+    //private var _thruParams = MIDIThruConnectionParams()
+    //private var _ids: MIDIUniqueID = 0xFF69b4
+    //private var _conRef = MIDIThruConnectionRef()
     private var _portRef = MIDIPortRef()
     
         /* Tests */
-    private var _midiThruDests: [MIDIThruConnectionEndpoint] = []
+    //private var _midiThruDests: [MIDIThruConnectionEndpoint] = []
     private var tmp: (MIDIDevice, MIDIPortRef)?
     
     init() {
         MIDIClientCreate("Noon client" as CFString, nil, nil, &_ref)
         MIDIOutputPortCreate(_ref, "Noon port" as CFString, &_portRef)
-        MIDIThruConnectionParamsInitialize(&_thruParams)
+        //MIDIThruConnectionParamsInitialize(&_thruParams)
 
         for index in 0 ... (self.numberOfDevices() - 1) {
             let ref = MIDIGetDevice(index)
             self._devices.append(MIDIDevice(id: index, name: _nameOf(device: ref), dests: _destsOf(device: ref)))
         }
-        _thruParams.numSources = 1
-        _thruParams.sources.0 = MIDIThruConnectionEndpoint(endpointRef: _edp, uniqueID: _ids)
-        _ids += 1;
+        //_thruParams.numSources = 1
+        //_thruParams.sources.0 = MIDIThruConnectionEndpoint(endpointRef: _edp, uniqueID: _ids)
+        //_ids += 1;
         
-        tmp = (_devices[6], _portRef)
-        MIDIDestinationCreate(_ref, "Noon endpoint" as CFString, { (pl, cls, c) -> Void in
-            guard let (dsts, port) = cls?.load(as: (MIDIDevice, MIDIPortRef).self) else { print("nil arg"); return }
-            for endpt in dsts.dests {
-                //MIDISend(port, endpt.ref, pl)
-            }
-        }, &tmp, &_edp)
-
-        withUnsafeMutablePointer(to: &_thruParams.destinations) { dst -> Void in
-            memcpy(dst, _midiThruDests, _midiThruDests.count) // Should be ok I guess
-            return
+        if let dev = _devices.first(where: { $0.dests.count == 4 }) {
+            tmp = (dev, _portRef)
+            MIDIDestinationCreate(_ref, "Noon endpoint" as CFString, { (pl, cls, c) -> Void in
+                guard let (dsts, port) = cls?.load(as: (MIDIDevice, MIDIPortRef).self) else { print("nil arg"); return }
+                for endpt in dsts.dests {
+                    MIDISend(port, endpt.ref, pl)
+                }
+            }, &tmp, &_edp)
         }
+        //withUnsafeMutablePointer(to: &_thruParams.destinations) { dst -> Void in
+            //memcpy(dst, _midiThruDests, _midiThruDests.count) // Should be ok I guess
+            //return
+        //}
         
-        let len = MIDIThruConnectionParamsSize(&self._thruParams)
-        let paramsData = withUnsafePointer(to: &_thruParams) { p in
-            NSData(bytes: p, length: len)
-        }
-        let res = MIDIThruConnectionCreate("com.appankarton.Noon" as CFString, paramsData, &_conRef) //TODO: Make that work or remove it
-        print("res = \(res)")
+        //let len = MIDIThruConnectionParamsSize(&self._thruParams)
+        //let paramsData = withUnsafePointer(to: &_thruParams) { p in
+            //NSData(bytes: p, length: len)
+        //}
+        //let res = MIDIThruConnectionCreate("com.appankarton.Noon" as CFString, paramsData, &_conRef) //TODO: Make that work or remove it
+        //print("res = \(res)")
     }
     
     deinit {
-        MIDIThruConnectionDispose(_conRef)
+        //MIDIThruConnectionDispose(_conRef)
         MIDIPortDispose(_portRef)
         MIDIClientDispose(_ref)
     }
@@ -114,13 +115,13 @@ class MIDIController {
                 let displayName = self._nameOf(device: dest)
                 if (displayName != "Error") { // TODO: Optional putain
                     ret.append(Dest(id: index, name: displayName, ref: dest));
-                    if (entitiesNb == 4) {
-                        _midiThruDests.append(MIDIThruConnectionEndpoint(endpointRef: _edp, uniqueID: self._ids))
-                        _thruParams.numDestinations += 1
-                        self._ids += 1
-                        print("Should be on teh interface need a button \(self._ids)")
+                    //if (entitiesNb == 4) {
+                        //_midiThruDests.append(MIDIThruConnectionEndpoint(endpointRef: _edp, uniqueID: self._ids))
+                        //_thruParams.numDestinations += 1
+                        //self._ids += 1
+                        //print("Should be on teh interface need a button \(self._ids)")
                         
-                    }
+                    //}
                 } else {
                     print("error motherf*cker")
                 }
